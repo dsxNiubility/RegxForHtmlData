c9f0d90bd22e47e1ac5605e27ac87ba10972dd6b
董铂然 <dantesx2012@gmail.com>
2015-02-20 15:24:45 +0800

    第一次上传
    准备工作完成

---
+++ 正则抓数据/94 - 正则抓数据/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  94 - 正则抓数据
+//
+//  Created by 董 尚先 on 15/2/20.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
